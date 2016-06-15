01032602c3ec58a83bb1831ec11c7afd4cfd4eeb
Jamaine01 <jroseborough01@gmail.com>
2016-06-15 10:05:04 -0400

    'update'

--- flask2/app/views.py
+++ flask2/app/views.py
@@ -11,5 +11,10 @@ def home():
 def game():
 	num_one = randint(20,100)
 	num_two = randint(20, 100)
-
+	if num_one > num_two:
+		print 'num_one == True'
+	elif num_two > num_one:
+		print 'num_two == True'
+	else:
+		print 'num_one and num_two == True'
 	return render_template('play.html', num_one = num_one, num_two = num_two)
